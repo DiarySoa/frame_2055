4160a0f591b5b27aa82bcb8587dc1c5ad62a87b1
DiarySoa <manjakarasolomanana@gmail.com>
2023-07-07 16:27:45 +0200

    144

--- framework/APIrest.java
+++ framework/APIrest.java
@@ -8,7 +8,6 @@ import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
 @Target(METHOD)
-public @interface Identification {
-	String user() default "";
+public @interface APIrest {
+	
 }
-
--- framework/Session.java
+++ framework/Session.java
@@ -1,14 +1,13 @@
 package etu2055.framework;
 
-import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
-@Target(METHOD)
-public @interface Identification {
-	String user() default "";
+@Target(ElementType.METHOD)
+public @interface Session {
 }
-
